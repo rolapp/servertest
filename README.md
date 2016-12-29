547c09bf46c6f8130e297d97f037b791b02b3a98
rolapp <github@rolapp.de>
2016-12-29 12:53:22 +0100

    Initial script

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # servertest
 small test if remote server online
+
+Put your config file to the folder server.d.
+	Filename must end with .config
+	IP=<server ip>
+	PORTS="80 22 .." Portnumbers seperate with space
+	MAIL=<your mail address" Email address for notification when a service is not available
+
+To protekt you from logdir overflow copy the file servertest to /etc/logrotate.d
+
+Create a cronjob like this "*/15 * * * *     /opt/servertest/servertest.sh > /dev/null 2>&1"
+
+log files you find in /var/log/servertest/<server-ip>
---
+++ servertest.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+#
+#  servertest.sh
+#  
+#  Copyright 2016 Steffen Rolapp <sw@rolapp.de>
+#  
+#  This program is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <http://www.gnu.org/licenses/
+#  
+#  
+
+
+#check if log dir exists
+
+LOGDIR="/var/log/servertest"
+if ! [ -d "$LOGDIR" ]
+		then
+			mkdir "$LOGDIR"
+fi
+# Check if there are any usable config files
+
+if ls $(dirname $0)/server.d/*.config 1> /dev/null 2>&1; then
+   
+   # Loop through configs
+   for f in $(dirname $0)/server.d/*.config
+   do
+	   source $f
+	   
+	   #test for logdir
+	   IPDIR="$LOGDIR/$IP"
+	   if ! [ -d "$IPDIR" ]
+		then
+			mkdir "$IPDIR"
+	   fi
+	   
+	   # old Log merge
+	   find $IPDIR -name 'test-*' | xargs cat >> $IPDIR/test.log
+	   echo >> $IPDIR/test.log
+	   echo `date +%Y-%m-%d_%H-%M` >> $IPDIR/test.log
+	   echo >> $IPDIR/test.log
+	   rm -rf $IPDIR/test-*.log
+	   
+	   LOG="$IPDIR/test-`date +%Y-%m-%d_%H-%M`.log"
+	   FAIL="$IPDIR/fail-`date +%Y-%m-%d_%H-%M`.log"
+	   
+	   nc -zv $IP $PORTS 2> $LOG
+	   cat $LOG | grep failed 1> /dev/null 2>&1
+	   if [ $? = 0 ]
+	   then
+		 cat $LOG | grep failed > $FAIL
+		 mail -s "Server ist down" $MAIL < $FAIL
+	   fi
+	
+	   unset IPDIR
+	   unset PORTS
+	   unset IP 
+	   unset MAIL
+	   unset LOG
+	   unset FAIL
+   done  
+else
+   echo "There does not seem to be any config file available in $(dirname $0)/server.d/." ; exit 1;
+fi
